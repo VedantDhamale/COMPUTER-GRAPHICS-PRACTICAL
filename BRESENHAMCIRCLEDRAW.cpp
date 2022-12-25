c49eb58a0d0f2f6034b12e66827abd2443e31695
Vedant <66168936+vedantdhamle9@users.noreply.github.com>
2022-12-26 00:31:57 +0530

    Update BRESENHAMCIRCLEDRAW.cpp

--- BRESENHAMCIRCLEDRAW.cpp
+++ BRESENHAMCIRCLEDRAW.cpp
@@ -2,8 +2,7 @@
 #include <graphics.h>
 
 using namespace std;
-// Function to put pixels
-// at subsequence points
+
 void drawCircle(int xc, int yc, int x, int y)
 {
 	putpixel(xc+x, yc+y, RED);
@@ -16,23 +15,14 @@ void drawCircle(int xc, int yc, int x, int y)
 	putpixel(xc-y, yc-x, RED);
 }
 
-// Function for circle-generation
-// using Bresenham's algorithm
 void circleBres(int xc, int yc, int r)
 {
 	int x = 0, y = r;
 	int d = 3 - 2 * r;
 	drawCircle(xc, yc, x, y);
 	while (y >= x)
-	{
-		// for each pixel we will
-		// draw all eight pixels
-		
+	{		
 		x++;
-
-		// check for decision parameter
-		// and correspondingly
-		// update d, x, y
 		if (d > 0)
 		{
 			y--;
